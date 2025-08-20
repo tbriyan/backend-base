763f5cecc5f630b17d08d867ebe311f16a7f2c66
iulius <tbriyan.dev@gmail.com>
2025-08-20 04:36:27 -0400

    feat: configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentación del proyecto')
+    .setDescription('Descripción minima necesaria para identificar al proyecto')
+    .setVersion('1.0.0')
+    .addTag('Prueba técnica project')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
